42abdb93e3fe99c5a3234cf45d58d489e8fd054c
Esther Roux <esther.roux13@gmail.com>
2016-12-21 15:40:22 +0100

    Scene Question

---
+++ app/Animo/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoad : MonoBehaviour {
+
+	public Canvas uiCanvas;
+	public Transform[] canvasChild;
+
+	// Use this for initialization
+	void Start () {
+		canvasChild= uiCanvas.GetComponentsInChildren<Transform>();
+
+
+		foreach (Transform child in canvasChild){
+
+		child.position-=Vector3.right * 663;
+		}
+	
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
