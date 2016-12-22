2673cdbfcc19fa5510857873e1c7d14acc44bdb8
tina28snu <tina28snu@yahoo.com>
2016-12-22 11:28:26 +0100

    start scene almost finished.

--- app/Animo/Assets/Scripts/NextButton.cs
+++ app/Animo/Assets/Scripts/NextButton.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement; 
 
 public class NextButton : MonoBehaviour {
-
+	public float timeBeforeLoadScene = 1f;
+	private float time = 0f;
 	public Animator animator;
+	public string sceneName;
 	// Use this for initialization
 	void Start () {
 
+	}
+
+	void Update(){
+		if (animator.GetBool ("click_on_next")) {
+			time += Time.deltaTime;
+			if (time >= timeBeforeLoadScene) {
+				SceneManager.LoadScene (sceneName);
+			}
+		}
 	}
 
 	public void ClickOnNext(){
