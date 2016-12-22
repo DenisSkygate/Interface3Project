800e52f18dbad5905482a422c33d6d2ae24d5a93
Sophie <sophie.roch@gmail.com>
2016-12-22 15:56:14 +0100

    stress gauge

---
+++ app/Animo/Assets/Scripts/ChangeSliderFillColor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeSliderFillColor : MonoBehaviour {
+
+	// to place on FillArea -> Fill
+
+	public Color[] colors;
+	Image fillImage;
+
+	// Use this for initialization
+	void Start () {
+		fillImage = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeColor (int fillingState) {
+		if (fillingState > 0) {
+			print(colors[fillingState - 1]);
+			fillImage.color = colors[fillingState - 1];
+			fillImage.SetAllDirty();
+		}
+	}
+}
---
+++ app/Animo/Assets/Scripts/StressManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class StressManager : MonoBehaviour {
+
+	public bool newGame = false;
+    
+    [System.Serializable]
+    public class OnStressLevelUpdate : UnityEvent<float> {
+
+    }
+    public OnStressLevelUpdate onStressLevelUpdate;
+
+	// Use this for initialization
+	void Start () {
+		if (newGame) {
+			InitializeStress();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitializeStress () {
+		PlayerPrefs.SetFloat("Stress", 0);
+	}
+
+	public void DecreaseStress (float amount) {
+		if (amount > 0) {
+			float stress = PlayerPrefs.GetFloat("Stress");
+			stress -= amount;
+			stress = Mathf.Max(stress, 0);
+			PlayerPrefs.SetFloat("Stress", stress);
+			onStressLevelUpdate.Invoke(stress);
+		} else {
+			print("Amount to decrease can't be negative");
+		}
+	}
+
+	public void IncreaseStress (float amount) {
+		if (amount > 0) {
+			float stress = PlayerPrefs.GetFloat("Stress");
+			stress += amount;
+			stress = Mathf.Min(stress, 100);
+			PlayerPrefs.SetFloat("Stress", stress);
+			onStressLevelUpdate.Invoke(stress);
+		} else {
+			print("Amount to increase can't be negative");
+		}
+	}
+}
