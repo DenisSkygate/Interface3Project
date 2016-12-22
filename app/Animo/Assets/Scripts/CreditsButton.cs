014937b713f88b8bb4f155560657084e5748f8ba
tina28snu <tina28snu@yahoo.com>
2016-12-22 15:51:44 +0100

    add credits and so many things

---
+++ app/Animo/Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MonoBehaviour {
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	public void ClickOnCredits(){
+		animator.SetBool ("click_on_credits", true);
+		animator.SetBool ("click_on_back", false);
+
+	}
+}
