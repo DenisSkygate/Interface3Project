9e41f69b23f3960e6bdfdb7056dc7998b3310de4
tina28snu <tina28snu@yahoo.com>
2016-12-22 11:02:39 +0100

    buttons worksgit add -Agit add -A

---
+++ app/Animo/Assets/Scripts/NextButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextButton : MonoBehaviour {
+
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void ClickOnNext(){
+		animator.SetBool ("click_on_next", true);
+	}
+}
---
+++ app/Animo/Assets/Scripts/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void ClickOnStart(){
+		animator.SetBool ("click_on_start", true);
+	}
+}
